e30841d96545ba1fa3e0d6e848260d6e304c49d4
KAVIN-KNIGHT <rkavin975rk@gmail.com>
2026-07-12 12:02:23 +0530

    mod added

--- Mod.java
+++ Mod.java
@@ -1,3 +1,10 @@
 public class Mod {
-    
+    public static int mod(int a, int b) {
+    if (a < b) {
+        return (b - a) % b;
+    } else {
+        int c = a % b;
+        return (b - c) % b;
+    }
+}
 }
